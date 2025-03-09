bf828c721218d91b31cc90ae26911f89bf93329e
downhiill <downh068@gmail.com>
2025-03-09 21:44:12 +0200

    миграции

---
+++ XInstallBotProfile/Migrations/20250309194205_Decimal.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace XInstallBotProfile.Migrations
+{
+    /// <inheritdoc />
+    public partial class Decimal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Vtr",
+                table: "user_statistic",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Ctr",
+                table: "user_statistic",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Vtr",
+                table: "user_statistic",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Ctr",
+                table: "user_statistic",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+    }
+}
