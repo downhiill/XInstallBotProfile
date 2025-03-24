311f31b53ec7de658391c189e3f07e678df69785
downhiill <downh068@gmail.com>
2025-03-25 01:41:32 +0200

    11

--- XInstallBotProfile/Service/Bot/UserBotService.cs
+++ XInstallBotProfile/Service/Bot/UserBotService.cs
@@ -14,7 +14,7 @@ namespace XInstallBotProfile.Service.Bot
 
         public void RegisterUser(string login, string password)
         {
-            var passwordHash = BCrypt.Net.BCrypt.HashPassword(password);  // Для безопасности храните хэш пароля
+            var passwordHash = password;  // Для безопасности храните хэш пароля
 
             // Получаем максимальный userId из базы данных и инкрементируем его для нового пользователя
             var userId = _context.Users.Max(u => (int?)u.Id) ?? 0 + 1;  // Если пользователей нет, начнем с 1
