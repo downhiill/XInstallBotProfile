87919e2586c9c4df8699b8f249088338efef46a4
downhiill <downh068@gmail.com>
2025-04-17 00:26:02 +0300

    миграция в бд

---
+++ XInstallBotProfile/Migrations/20250416211832_SwitchTypeDecimalInColumnComplitedTableXInstallApp.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace XInstallBotProfile.Migrations
+{
+    /// <inheritdoc />
+    public partial class SwitchTypeDecimalInColumnComplitedTableXInstallApp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Complited",
+                table: "xinstallapp_user_stat",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Complited",
+                table: "xinstallapp_user_stat",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+    }
+}
